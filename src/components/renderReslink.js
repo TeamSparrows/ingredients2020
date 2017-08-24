7de8b6c24d75f40a7ae0ef87d5a5c8caa36527be
d-kang <david.kang714@gmail.com>
2017-08-24 08:15:51 -0700

    Add renderReslink component

---
+++ src/components/renderReslink.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const RenderReslink = (props) => {
+
+  return (
+      props.searchResLink
+        ? <div>{  props.searchResName + ' found in database! - '}
+            <a href={props.searchResLink} target="_blank">{props.searchResLink }</a>
+          </div>
+        : <div>{props.searchResName}</div>
+  )
+}
+
+export default RenderReslink;
