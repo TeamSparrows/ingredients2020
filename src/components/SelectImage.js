ef4552a7e5cb2296125e7798bbc38c0651bb240f
d-kang <david.kang714@gmail.com>
2017-08-24 07:03:19 -0700

    Add Handle Submit

--- src/components/SelectImage.js
+++ src/components/SelectImage.js
@@ -1,17 +1,83 @@
 import React, { Component } from 'react';
-
+import $ from 'jquery';
 class SelectImage extends Component {
+  
+  handleSubmit(e) {
+    e.preventDefault();
+    const _this = this;
+    const obj = {
+      search: '',
+      searchResName: '',
+      searchResLink: '',
+      currentFlagged: [],
+      processing: false,
+      passed: '',
+      pastSearches: [],
+    }
+    console.log('obj', JSON.stringify(obj, null, 2));
+    _this.setState(obj)
+
+
+    var data = {
+      data_uri: this.state.data_uri,
+      filename: this.state.filename,
+      filetype: this.state.filetype,
+      username: this.state.username
+    }
+
+    $.ajax({
+      url: '/api/image',
+      type: 'POST',
+      data: data,
+      dataType: 'json'
+    })
+    .done(function(data){
+      console.log(data);
+      if(data.slice(1).length === 0){
+        _this.setState({
+          passed: 'No Flagged Ingredients, feel free to gobble this up!'
+        })
+      }
+      _this.setState({
+        currentFlagged: data
+      });
+    })
+  }
+
+  handleFile(e) {
+    this.setState({
+      search: '',
+      searchResName: '',
+      searchResLink: '',
+      currentFlagged: [],
+      processing: false,
+      passed: '',
+      pastSearches: [],
+    })
+    const reader = new FileReader();
+    const file = e.target.files[0];
+
+    reader.onload = (upload) => {
+      this.setState({
+        data_uri: upload.target.result,
+        filename: file.name,
+        filetype: file.type
+      });
+    };
+    reader.readAsDataURL(file);
+  }
+
   render() {
     return (
       <form
         className="form-control"
-        onSubmit={this.props.handleSubmit}
+        onSubmit={this.handleSubmit.bind(this)}
         encType='multipart/form-data'>
         <input
           className="custom-file-input"
           type='file'
           name='image'
-          onChange={this.props.handleFile} />
+          onChange={this.handleFile.bind(this)} />
         <input
           className="Submit-btn neg-margin-t"
           type="submit"
