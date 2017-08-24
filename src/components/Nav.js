00f51a37d80fe6199f708284e33c8a26062a2847
d-kang <david.kang714@gmail.com>
2017-08-24 08:04:46 -0700

    Pass Logout Method to Navbar as Props

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-const Nav = () => {
+import React from 'react';
+const Nav = (props) => {
   return (
     <div>
-      <button className="Logout-btn" onClick={this.logout}>LOG OUT</button>
+      <button className="Logout-btn" onClick={props.logout}>LOG OUT</button>
       <h2 className="App-header">Ingredients 20/20</h2>
     </div>
   )
