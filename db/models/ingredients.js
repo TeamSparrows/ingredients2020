c99a818fd8de433cc4bfb67bb063cd0b2464b0c3
d-kang <david.kang714@gmail.com>
2017-08-23 21:20:48 -0700

    Require Mongoose Connection

--- db/models/ingredients.js
+++ db/models/ingredients.js
@@ -1,10 +1,11 @@
-var mongoose = require('mongoose');
-
-var ingredientsSchema = mongoose.Schema({
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('../config');
+const ingredientsSchema = mongoose.Schema({
   name: { type: String, required: true },
   link: { type: String, required: true }
 }, {collection: 'Ingredients'});
 
-var Ingredient = mongoose.model('Ingredient', ingredientsSchema);
+const Ingredient = mongoose.model('Ingredient', ingredientsSchema);
 
 module.exports = Ingredient;
